65fca370a06a66de2bd5991601dc95efd669d4df
Daont1610 <42923011+Daont1610@users.noreply.github.com>
2019-04-03 10:55:18 +0700

    commit BaiAlert.js

---
+++ BaiAlert.js
@@ -0,0 +1,17 @@
+// alert("Hello worl");
+// alert(1993);
+// alert(true);
+
+// var result= confirm("Are you sure want to delete?");
+// if(result==true){
+//     alert("User accept");
+// }else{
+//     alert("User deny");
+// }
+
+var input= prompt('Please enter your name', ' no name');
+if(input != null){
+    alert(input);
+}else{
+    
+}
